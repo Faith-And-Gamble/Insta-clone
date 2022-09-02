157478d1402969ab6d87a39ed7ed64b03ed986b8
Your Name <hudsasharma10@gmail.com>
2022-09-02 08:17:18 +0530

    ading structure

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = express();
-const connectToDB = require('./db/index');
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const connectToDB = require('./db/index')
+const PORT = 3000
 
-connectToDB();
+connectToDB()
 
-app.listen(PORT, console.log("server is listening on port",PORT));
+app.listen(PORT, console.log('Heya...! Server is listening on port', PORT))
