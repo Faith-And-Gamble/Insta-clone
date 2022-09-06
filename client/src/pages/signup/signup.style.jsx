25eb32e8ecbe48c4d5cf78499ba346c4109e19ae
Priyansh <104319269+priyanshnahar@users.noreply.github.com>
2022-09-06 08:31:27 +0530

    Adding | signup (#4)

---
+++ client/src/pages/signup/signup.style.jsx
@@ -0,0 +1,16 @@
+// import styled from 'styled-components';
+
+// export const Main = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+// `
+
+// // export const Page = styled.div`
+// //     display: flex;
+// //     flex-direction: column;
+// //     align-items: center;
+// //     padding-bottom: 30px;
+// // `
+
+
